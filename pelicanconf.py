1cd60aaa3b37ae6759a66c2f50d3fc2d179e940c
Alexis M <alexis@notmyidea.org>
2019-11-17 01:30:38 +0100

    Ajout d'un menu.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,33 +2,41 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHORS = (
-    u'Le CRAB',
-)
+AUTHORS = ("Le CRAB",)
 
-SITENAME = u'CRAB - Collectif Rennais des Amateurs de Bières'
-SITEURL = 'http://localhost:8000'
+SITENAME = "CRAB - Collectif Rennais des Amateurs de Bières"
+SITEURL = "http://localhost:8000"
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Europe/Paris'
-DEFAULT_DATE_FORMAT = ('%d %B %Y')
+TIMEZONE = "Europe/Paris"
+DEFAULT_DATE_FORMAT = "%d %B %Y"
 
-DEFAULT_LANG = u'fr'
+DEFAULT_LANG = "fr"
 
 DEFAULT_PAGINATION = False
 
 THEME = "theme"
 
 MENUITEMS = (
-    (u'Accueil', '/index.html', 'wide'),
-    (u'Actus', '/blog.html', 'mobile'),
+    ("Accueil", "/index.html"),
+    ("Actus", "/blog.html"),
+    (
+        "Adhérer",
+        "https://www.helloasso.com/associations/collectif-rennais-des-amateurs-de-bieres-crab/adhesions/adhesion-au-crab",
+    ),
 )
 DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = False
-DIRECT_TEMPLATES = ['index', 'blog']
-STATIC_PATHS = ['images', 'assets', 'extra/CNAME', ]
-EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
-PLUGIN_PATHS = ["plugins", ]
+DIRECT_TEMPLATES = ["index", "blog"]
+STATIC_PATHS = [
+    "images",
+    "assets",
+    "extra/CNAME",
+]
+EXTRA_PATH_METADATA = {"extra/CNAME": {"path": "CNAME"}}
+PLUGIN_PATHS = [
+    "plugins",
+]
 
 RELATIVE_URLS = True
